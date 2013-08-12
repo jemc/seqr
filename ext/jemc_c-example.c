5a79bf41cbecb9a2e1279c5167b74cdaf0f09a32
Joe McIlvain <joe.eli.mac@gmail.com>
2013-08-12 14:52:45 -0400

    Added skeleton init code to c file

--- ext/jemc_c-example.c
+++ ext/jemc_c-example.c
@@ -1,2 +1,16 @@
 
 #include <ruby.h>
+
+
+VALUE JEMC     = Qnil;
+VALUE CExample = Qnil;
+
+
+void Init_JEMC_CExample();
+
+
+void Init_JEMC_CExample()
+{
+  JEMC     = rb_define_module("JEMC");
+  CExample = rb_define_module_under(JEMC, "CExample");
+}
