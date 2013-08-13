3c0807051ee8750f95f913076325957ec2ecd2f2
Joe McIlvain <joe.eli.mac@gmail.com>
2013-08-12 21:48:39 -0400

    Added JEMC::CExample.hello method, returns nothing

--- ext/jemc_cexample/jemc_cexample.c
+++ ext/jemc_cexample/jemc_cexample.c
@@ -3,14 +3,24 @@
 
 
 VALUE JEMC     = Qnil;
-VALUE CExample = Qnil;
+VALUE JEMC_CExample = Qnil;
 
 
 void Init_jemc_cexample();
+void m_JEMC_CExample_hello();
 
 
 void Init_jemc_cexample()
 {
-  JEMC     = rb_define_module("JEMC");
-  CExample = rb_define_module_under(JEMC, "CExample");
+  JEMC          = rb_define_module("JEMC");
+  JEMC_CExample = rb_define_module_under(JEMC, "CExample");
+  
+  rb_define_singleton_method(JEMC_CExample,"hello",
+                           m_JEMC_CExample_hello, 0);
+}
+
+
+void m_JEMC_CExample_hello()
+{
+  return;
 }
