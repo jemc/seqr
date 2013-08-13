103d88164a9043f2424d98a5c5b91a6509664536
Joe McIlvain <joe.eli.mac@gmail.com>
2013-08-12 21:51:49 -0400

    Renamed '.hello' to '.dummy' because it returns nothing

--- ext/jemc_cexample/jemc_cexample.c
+++ ext/jemc_cexample/jemc_cexample.c
@@ -7,7 +7,7 @@ VALUE JEMC_CExample = Qnil;
 
 
 void Init_jemc_cexample();
-void m_JEMC_CExample_hello();
+void m_JEMC_CExample_dummy();
 
 
 void Init_jemc_cexample()
@@ -15,12 +15,12 @@ void Init_jemc_cexample()
   JEMC          = rb_define_module("JEMC");
   JEMC_CExample = rb_define_module_under(JEMC, "CExample");
   
-  rb_define_singleton_method(JEMC_CExample,"hello",
-                           m_JEMC_CExample_hello, 0);
+  rb_define_singleton_method(JEMC_CExample,"dummy",
+                           m_JEMC_CExample_dummy, 0);
 }
 
 
-void m_JEMC_CExample_hello()
+void m_JEMC_CExample_dummy()
 {
   return;
 }
--- sandbox.rb
+++ sandbox.rb
@@ -2,3 +2,4 @@ require 'jemc_cexample'
 
 p JEMC::CExample
 p JEMC::CExample.singleton_methods(false)
+p JEMC::CExample.dummy
