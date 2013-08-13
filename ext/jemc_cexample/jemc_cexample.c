3c7c7037b7304c0accafe96bd1e5ea81af870ce2
Joe McIlvain <joe.eli.mac@gmail.com>
2013-08-12 22:08:20 -0400

    Added version function to return a string for the version number

--- ext/jemc_cexample/jemc_cexample.c
+++ ext/jemc_cexample/jemc_cexample.c
@@ -1,13 +1,17 @@
 
 #include <ruby.h>
 
+const char JEMC_CEXAMPLE_VERSION[] = "0.0.0";
 
-VALUE JEMC     = Qnil;
+
+VALUE JEMC          = Qnil;
 VALUE JEMC_CExample = Qnil;
 
 
 void Init_jemc_cexample();
-void m_JEMC_CExample_dummy();
+
+VALUE m_JEMC_CExample_dummy();
+VALUE m_JEMC_CExample_version();
 
 
 void Init_jemc_cexample()
@@ -17,10 +21,17 @@ void Init_jemc_cexample()
   
   rb_define_singleton_method(JEMC_CExample,"dummy",
                            m_JEMC_CExample_dummy, 0);
+  rb_define_singleton_method(JEMC_CExample,"version",
+                           m_JEMC_CExample_version, 0);
 }
 
 
-void m_JEMC_CExample_dummy()
+VALUE m_JEMC_CExample_dummy()
 {
-  return;
+  return Qnil;
+}
+
+VALUE m_JEMC_CExample_version()
+{
+  return rb_str_new2(JEMC_CEXAMPLE_VERSION);
 }
--- sandbox.rb
+++ sandbox.rb
@@ -3,3 +3,4 @@ require 'jemc_cexample'
 p JEMC::CExample
 p JEMC::CExample.singleton_methods(false)
 p JEMC::CExample.dummy
+p JEMC::CExample.version
