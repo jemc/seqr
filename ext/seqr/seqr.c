f9204beedc23a67b761e0e0e15a9aa9ee5782062
Joe McIlvain <joe.eli.mac@gmail.com>
2013-10-08 22:32:20 -0400

    JackClient now more object oriented.

--- ext/seqr/seqr.c
+++ ext/seqr/seqr.c
@@ -10,12 +10,28 @@ VALUE Seqr           = Qnil;
     VALUE JackClient = Qnil;
 
 
-jack_client_t* the_client;
+static jack_client_t* JackClient_get(VALUE self);
+VALUE JackClient_k_alloc(VALUE klass);
+VALUE JackClient_m_initialize(VALUE self);
+VALUE JackClient_m_close(VALUE self);
 
+
+static jack_client_t* JackClient_get(VALUE self)
+{
+  jack_client_t* ptr;
+  Data_Get_Struct(self, jack_client_t, ptr);
+  return ptr;
+}
+
+VALUE JackClient_k_alloc(VALUE klass)
+{
+  jack_client_t* ptr;
+  ptr = jack_client_open("dog", JackNullOption, NULL);
+  return Data_Wrap_Struct(klass, NULL, NULL, ptr);
+}
 
 VALUE JackClient_m_initialize(VALUE self)
 {
-  the_client = jack_client_open("dog", JackNullOption, NULL);
   rb_iv_set(self, "@open", Qtrue);
   return self;
 }
@@ -23,19 +39,8 @@ VALUE JackClient_m_initialize(VALUE self)
 VALUE JackClient_m_close(VALUE self)
 {
   rb_iv_set(self, "@open", Qfalse);
-  return INT2NUM(jack_client_close((jack_client_t *)the_client));
+  return INT2NUM(jack_client_close(JackClient_get(self)));
 }
-
-
-// VALUE m_JEMC_CExample_version(VALUE self)
-// {
-//   return rb_str_new2(JEMC_CEXAMPLE_VERSION);
-// }
-
-// VALUE m_JEMC_CExample_passthru(VALUE self, VALUE val)
-// {
-//   return val;
-// }
 
 
 void Init_seqr()
@@ -44,12 +49,9 @@ void Init_seqr()
   Ext        = rb_define_module_under(Seqr, "Ext");
   JackClient = rb_define_class_under(Ext, "JackClient", rb_cObject);
   
+  rb_define_alloc_func(JackClient, JackClient_k_alloc);
   rb_define_method(JackClient, "initialize",
                    JackClient_m_initialize, 0);
   rb_define_method(JackClient, "close",
                    JackClient_m_close, 0);
-  // rb_define_singleton_method(JEMC_CExample,"version",
-  //                          m_JEMC_CExample_version, 0);
-  // rb_define_singleton_method(JEMC_CExample,"passthru",
-  //                          m_JEMC_CExample_passthru, 1);
 }
