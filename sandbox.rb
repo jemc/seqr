fc27f17d1a411f96aa25036b01488e545fb1537b
Joe McIlvain <joe.eli.mac@gmail.com>
2013-10-10 23:52:40 -0400

    Preempted seqr definitions with ruby

--- sandbox.rb
+++ sandbox.rb
@@ -1,33 +1,38 @@
-require 'seqr'
-include Seqr
 
 at_exit {`killall jackd`}
 
-module Jack
-  
-  class Client
-    def initialize(name="Jack::Client", options=Jack::Options::NullOption)
-      status = open name, options
-      if status & Status::Failure
-        raise StatusError.new("Failed to open the Jack::Client", status)
+module Seqr
+  module Jack
+    
+    class Client
+      def initialize(name="Jack::Client", options=Jack::Options::NullOption)
+        status = open name, options
+        if status & Status::Failure
+          raise StatusError.new("Failed to open the Jack::Client", status)
+        end
       end
     end
-  end
-  
-  class Error < RuntimeError; end
-  
-  class StatusError < Error
-    attr_reader :status
     
-    def initialize(str="", status=nil)
-      super(str)
-      @status = status
+    class Error < RuntimeError; end
+    
+    class StatusError < Error
+      attr_reader :status
+      
+      def initialize(str="", status=nil)
+        super(str)
+        @status = status
+      end
     end
+    
   end
-  
 end
 
+require 'seqr'
+include Seqr
 
-p Jack::Client.new.open("Jack::Client",Jack::Options::NullOption)
-p Jack::Client.new.open('dog',Jack::Options::NullOption)
-p Jack::Client.new.open('dog',Jack::Options::UseExactName)
+
+
+p Jack::Client.new
+p Jack::Client.new('dog')
+sleep 0.5
+p Jack::Client.new('dog',Jack::Options::UseExactName)
