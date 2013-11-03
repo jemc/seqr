83396c42a64a5c2f9a0bc9b914e92cbe4a957307
Joe McIlvain <joe.eli.mac@gmail.com>
2013-11-02 23:29:16 -0400

    Give GainNode proper garbage collection - need to figure out how to encapsulate it into CPP2RB_P macros

--- ext/seqr/node/gain.hpp
+++ ext/seqr/node/gain.hpp
@@ -7,6 +7,8 @@ class GainNode : public Node {
   public:
     CPP2RB_P_MEMBER(gain, double, 1.0);
     
+    virtual void cpp2rb_mark();
+    
     virtual audio_sample_t* get_buffer(nframes_t nframes);
 };
 CPP2RB_W_FUNCS(GainNode);
@@ -15,6 +17,13 @@ CPP2RB_P_FUNCS(GainNode, gain, NUM2DBL);
 
 ///
 // C++ methods
+
+// Garbage collection marker
+void GainNode::cpp2rb_mark()
+{
+  Node::cpp2rb_mark();
+  rb_gc_mark(this->rb_gain);
+}
 
 audio_sample_t* GainNode::get_buffer(nframes_t nframes)
 {
